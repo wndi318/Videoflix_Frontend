2d35c9e19a388492f66c3a3a047d05e1dd6bbe92
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-09-04 13:28:29 +0200

    added login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../shared/navbar/navbar.component";
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    MatCardModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  passwordFieldType: string = 'password';
 
+  togglePasswordVisibility(event: MouseEvent): void {
+    const inputField = event.target as HTMLInputElement;
+
+    // Berechne, ob der Klick im Bereich des Sichtbarkeits-Symbols war
+    const inputWidth = inputField.offsetWidth;
+    const clickPosition = event.clientX - inputField.getBoundingClientRect().left;
+    const iconPositionStart = inputWidth - 40; // Position des Icons basierend auf background-position und padding
+
+    if (clickPosition >= iconPositionStart) {
+      // Wenn in den Bereich des Icons geklickt wurde, Passwort anzeigen/ausblenden
+      this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password';
+    }
+  }
 }
