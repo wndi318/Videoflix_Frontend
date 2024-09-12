900ced609824fa5698847058207a84b18541e181
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-09-12 10:44:35 +0200

    added register logic

--- src/app/startpage-components/sign-up/sign-up.component.ts
+++ src/app/startpage-components/sign-up/sign-up.component.ts
@@ -1,19 +1,56 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
   imports: [
     MatCardModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
   passwordFieldType: string = 'password';
+
+  constructor(private authService: AuthService, private router: Router) { }
+  passwordMismatch(): boolean {
+    return this.password !== this.confirmPassword;
+  }
+
+  formValid(): boolean {
+    return this.email !== '' && this.password !== '' && !this.passwordMismatch();
+  }
+
+  register(): void {
+    const userData = {
+      email: this.email,
+      password: this.password
+    };
+
+    this.authService.registerUser(userData).subscribe({
+      next: (response) => {
+        console.log('Registration successful', response);
+      },
+      error: (error) => {
+        console.error('Registration failed', error);
+      },
+      complete: () => {
+        console.log('Registration process completed.');
+      }
+    });
+  }
 
   togglePasswordVisibility(event: MouseEvent): void {
     const inputField = event.target as HTMLInputElement;
