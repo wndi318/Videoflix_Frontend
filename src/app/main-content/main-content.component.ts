3e3415be0ef37a6898ce07915f7d592696988d57
Steffen Winter <155267553+wndi318@users.noreply.github.com>
2024-09-16 18:46:41 +0200

    added logout funktion at main content component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-main-content',
@@ -11,5 +12,25 @@ import { RouterModule } from '@angular/router';
   styleUrl: './main-content.component.scss'
 })
 export class MainContentComponent {
+
+  constructor(private authService: AuthService, private router: Router) { }
+  
+  logout(): void {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authService.logoutUser(token).subscribe({
+        next: (response) => {
+          console.log('Logout successful', response);
+          localStorage.removeItem('token');
+          this.router.navigate(['/log-in']);
+        },
+        error: (error) => {
+          console.log('Logout failed', error);
+        }
+      });
+    } else {
+      console.log('No token found, user not logged in.');
+    }
+  }
 
 }
